cebecf9a5b5ecb543185069cb78c5d861eeb1b07
Amit Tauro <tauroamit@gmail.com>
2021-02-08 14:29:01 +0000

    linting

--- src/__tests__/FileInput.test.js
+++ src/__tests__/FileInput.test.js
@@ -4,14 +4,14 @@ import userEvent from '@testing-library/user-event'
 
 test('renders file uploading', () => {
   render(<FileInput />)
-  const fileSubmit = screen.getByRole('button', {name: /submit/i})
+  const fileSubmit = screen.getByRole('button', { name: /submit/i })
   expect(fileSubmit).toBeInTheDocument()
 })
 
 test('submitting file', () => {
-  const readAsText = jest.spyOn(FileReader.prototype, 'readAsText').mockImplementation();
+  const readAsText = jest.spyOn(FileReader.prototype, 'readAsText').mockImplementation()
   render(<FileInput />)
-  const fileSubmit = screen.getByRole('button', {name: /submit/i})
+  const fileSubmit = screen.getByRole('button', { name: /submit/i })
   userEvent.click(fileSubmit)
   expect(readAsText).toHaveBeenCalled()
 })
