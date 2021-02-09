f873ef2fb4dc9fe4738de017f9ffa6692290c6fe
Amit Tauro <tauroamit@gmail.com>
2021-02-09 09:04:28 +0000

    trying to test file submit

--- src/__tests__/FileSubmit.test.js
+++ src/__tests__/FileSubmit.test.js
@@ -10,18 +10,25 @@ test('renders file uploading', () => {
 })
 
 test('submitting file', async () => {
+
   const data = [{
     Date: 'fake',
     Description: 'fake1',
     Amount: -15.75,
     Balance: 'balance'
   }]
+  const text = jest.spyOn(FileReader.prototype, 'text')
   jest.mock("../Parser", () => ({data}))
-  const readAsText = jest.spyOn(FileReader.prototype, 'readAsText').mockImplementation()
+
+  // jest.spyOn(FileReader.prototype, 'onload')
+
+  // .mockImplementation(() => {
+  //   return Promise.resolve(data)
+  // })
   render(<FileSubmit />)
   const fileSubmit = screen.getByRole('button', { name: /submit/i })
   userEvent.click(fileSubmit)
-  expect(readAsText).toHaveBeenCalled()
+  expect(text).toHaveBeenCalled()
   screen.debug()
   const element = await waitFor(() => screen.getByText(/description/))
   expect(element).toBeInTheDocument()
